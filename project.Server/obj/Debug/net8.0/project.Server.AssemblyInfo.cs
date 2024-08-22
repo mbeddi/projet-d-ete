4070ae90d3855bb8cfc6ea326ab96830bd1742f1
medbeddi <beddimed56@gmail.com>
2024-08-22 03:13:26 +0100

    Added project files

--- project.Server/obj/Debug/net8.0/project.Server.AssemblyInfo.cs
+++ project.Server/obj/Debug/net8.0/project.Server.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7cce5b84-5132-4ed7-90fe-4966123a25a6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("project.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620ee68280a54610e58f21bf46c08c8e63194ab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("project.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
